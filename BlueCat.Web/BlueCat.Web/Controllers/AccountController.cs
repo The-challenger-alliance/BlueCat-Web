783d6153164745c678f7028500807e95f07c6be8
XULIANGJIE-PC\亮杰 <xuliangjie19870208@163.com>
2016-12-10 20:28:29 +0800

    Add  form authorize.

--- BlueCat.Web/BlueCat.Web/Controllers/AccountController.cs
+++ BlueCat.Web/BlueCat.Web/Controllers/AccountController.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Transactions;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Security;
-using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
-using WebMatrix.WebData;
-using BlueCat.Web.Filters;
-using BlueCat.Web.Models;
+﻿using BlueCat.Model;
 using BlueCat.Web.Common;
-using BlueCat.Model;
+using System.Web.Mvc;
 
 namespace BlueCat.Web.Controllers
 {
---
+++ BlueCat.Web/BlueCat.Web/Controllers/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using BlueCat.Core;
+using BlueCat.Model;
+using BlueCat.Web.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlueCat.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public UserModel CurrentBlueCatUser
+        {
+            get
+            {
+                return SessionManager.User;
+            }
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var noAuthorizeAttributes = filterContext.ActionDescriptor.GetCustomAttributes(typeof(AuthorizeIgnoreAttribute), false);
+
+            if (noAuthorizeAttributes.Length > 0) return;
+            
+            base.OnActionExecuting(filterContext);
+
+            if (this.CurrentBlueCatUser == null)
+            {
+                filterContext.HttpContext.Response.StatusCode = 403;
+                filterContext.HttpContext.Response.StatusDescription = "needs login";
+                return;
+            }
+        }
+    }
+}
